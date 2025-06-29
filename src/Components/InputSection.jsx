10f2254398635de0a62673d723cc994defcaab76
Satyam Revgade <satyamrevgade2@gmail.com>
2025-06-29 21:25:08 +0530

    completed input section for md size screens

--- src/Components/InputSection.jsx
+++ src/Components/InputSection.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import Styles from "./InputSection.module.css"
+import { IoAdd } from "react-icons/io5";
 function InputSection(){
     return(
         <div className="container">
             <div className="row">
-                <div className = "col-6 col-md-6">
-                    col1
+                <div className={`col-6 col-md-6 ${Styles["task-input-container"]} `}>
+
+                    <input className= {Styles["task-input"]} type = "text" placeholder="Add your task here"/>
 
                 </div>
-                <div className="col-6 col-md-4">
-                    col2
+                <div className= {`col-6 col-md-4  ${Styles["date-input-container"]}`}>
+                    
+                      <input className= {Styles["date-input"]} type = "date" />
 
                 </div>
-                <div className="col-2 d-none d-md-block">
-                    col3
+                <div className={`d-none col-md-2 d-md-block ${Styles["task-input-container"]} `}>
+                    <button className= {Styles.button}>Add Task{<IoAdd className={Styles["add-button"]}/>}</button>
+                    
 
                 </div>
 
